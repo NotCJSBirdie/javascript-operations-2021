eeaccf681cd84ddae93bca6ce44c09df76ead344
Carl Serquiña <serquinajhunlo@gmail.com>
2021-10-18 11:50:56 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//This is an external Javascript File//
+
+function myfunction() {
+  let text1 = "Carl";
+  let text2 = "Serquina";
+  let text3 = text1 + " " + text2;
+  document.getElementById("p2").innerHTML = text3;
+}
